5237b4601168b1570795e87406306b4cb1cdb333
Yuliia Afanasenko <yulia.a.a@outlook.com>
2017-09-06 13:28:44 +0300

    Fixed style for Divider

--- src/editable/html/containers/Divider.js
+++ src/editable/html/containers/Divider.js
@@ -7,14 +7,17 @@ export const Divider = ({ section, index, props, context, pos, childIndex }) => 
   const sp = { props, context, pos, childIndex };
   const { styles, classes } = getStyling({ ...sp, styling: ['Block', 'Visibility'] });
   if (styles === false) return false;
-  //
   styles.width = '100%';
+  styles.fontSize = 0;
+  if (!props.textAlign) {
+    styles.textAlign = 'center';
+  }
   const innerStyle = {
-    margin: '0px auto',
+    display: 'inline-block',
+    verticalAlign: 'middle',
     width: props.width ? props.width : '100%',
     height: props.height ? props.height : 2,
-    background: props.borderColor ? props.borderColor : '#888',
-    float: props.textAlign ? props.textAlign : 'none'
+    background: props.borderColor ? props.borderColor : '#888'
   };
   return (
     <div key={index} className={classes.join(' ')} style={styles}>
