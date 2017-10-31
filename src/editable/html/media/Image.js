21e8a0b35802eb5e21c070c51bc74969ff570e8e
Ivan Kalinichenko <deepseer.ivan@outlook.com>
2017-10-31 11:16:30 +0200

    Added alternative image feature for Image component

--- src/editable/html/media/Image.js
+++ src/editable/html/media/Image.js
@@ -5,16 +5,22 @@ export const Image = ({ section, index, props, context, pos, childIndex }) => {
   Logger.of('render.html.Image').info('section', section, 'index', index, 'props', props, 'pos', pos);
   const sp = { props, context, pos, childIndex };
   const { styles, classes } = getStyling({
-    ...sp, mandatory: ['src'], optional: ['title', 'sizes', 'srcSet'], styling: ['Inline', 'Visibility']
+    ...sp, mandatory: ['src'], optional: ['title', 'sizes', 'srcSet', 'srcAlt'], styling: ['Inline', 'Visibility']
   });
   if (styles === false) return false;
   //
   const src = getValue(props, 'src', context);
+  const srcAlt = getValue(props, 'srcAlt', context);
   if (typeof src === 'undefined') {
     Logger.of('render.html.Image').info('Image is missing src property, props=', props, 'context=', context);
     return false;
   }
   if (!src) return false;
+  const onError = (e) => {
+    if (typeof srcAlt !== 'undefined') {
+      e.target.src = srcAlt;
+    }
+  };
   return (
     <img
       style={styles}
@@ -22,6 +28,7 @@ export const Image = ({ section, index, props, context, pos, childIndex }) => {
       src={src}
       sizes={props.sizes}
       srcSet={props.srcset}
+      onError={onError}
       alt={getValue(props, 'title', context)}
     />
   );
